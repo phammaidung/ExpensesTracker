efb13f98f51f9a0e945f12a9aad0698563148c1e
dungphamWeallnet <dung.pham@weallnet.com>
2023-07-31 18:11:30 +0700

    create model overlay for adding new expense

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -13,6 +14,6 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (context, index) => Text(expenses[index].title));
+        itemBuilder: (context, index) => ExpenseItem(expenses[index]));
   }
 }
